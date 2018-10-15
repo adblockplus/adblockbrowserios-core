24233904f097aa0c18243b799d74e811027288aa
Andy Shephard <a.shephard@eyeo.com>
2018-10-15 11:21:16 +0200

    eyeo/auxiliary/issue#167 - Adding nullable or nonnull properties to other functions to fix warnings.

--- src/Utils.m
+++ src/Utils.m
@@ -171,12 +171,12 @@ static NSRegularExpression *_rexWhitespaces;
     }
 }
 
-+ (NSString *)applicationName
++ (NSString *_Nonnull)applicationName
 {
     return [[NSBundle mainBundle] objectForInfoDictionaryKey:@"CFBundleName"];
 }
 
-+ (BOOL)isObjectReferenceNil:(id)reference
++ (BOOL)isObjectReferenceNil:(id _Nullable )reference
 {
     return reference == nil;
 }
